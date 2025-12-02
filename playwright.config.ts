a02ec1d62c3e1c7c06e65a058e28a9fbfffc840d
Lakshmi-ETS <Lakshmi.sathish@theeminence.org>
2025-12-02 11:52:56 +0530

    changed  config yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,24 +1,24 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : 1,
+
+  // Reporter config: HTML report in each suite folder
   reporter: [
-    // Generate HTML reports in "results/<suite>" folder dynamically
     ['html', { outputFolder: 'results/playwright-report', open: 'never' }]
   ],
+
   use: {
     headless: true,
-    trace: 'on-first-retry',
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    trace: 'on-first-retry',           // Trace only on retry
+    screenshot: 'only-on-failure',     // Screenshot on failure
+    video: 'retain-on-failure',        // Video on failure
   },
+
   projects: [
     {
       name: 'Google Chrome',
